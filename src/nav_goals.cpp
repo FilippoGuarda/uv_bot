aa3f81b22317933c9f9cf6c2845762fd04d117e6
Filippo Guarda <filippo.guarda96@gmail.com>
2022-03-21 00:55:34 +0100

    nav goals can import positions from file

--- src/nav_goals.cpp
+++ src/nav_goals.cpp
@@ -1,43 +1,54 @@
-#include <ros/ros.h> 
+#include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
-#include "file_reader.cpp"
+#include <string>
+#include "file_reader_class.cpp"
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+using namespace std;
 
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
 
     ros::init(argc, argv, "nav_goals");
 
     // we want to spin a thread by default
     MoveBaseClient ac("move_base", true);
 
-    //wait for action server to come up
-    while(!ac.waitForServer(ros::Duration(5.0))){
+    // wait for action server to come up
+    while (!ac.waitForServer(ros::Duration(5.0)))
+    {
         ROS_INFO("Waiting for the move_base action server to come up");
     }
 
-move_base_msgs::MoveBaseGoal goal;
+    move_base_msgs::MoveBaseGoal goal;
 
-std
+    GetRooms room;
+    room.readFile("test.txt");
+    vector<roomData> rooms = room.getRooms();
+    int i = 0;
 
-//we'll send a goal to the robot to move 1 meter forward
-while(read_positions){
-    goal.target_pose.header.frame_id = "base_link";
-    goal.target_pose.header.stamp = ros::Time::now();
+    // we'll send a goal to the robot to move 1 meter forward
+    while (rooms[i].Name != "0")
+    {
+        goal.target_pose.header.frame_id = "base_link";
+        goal.target_pose.header.stamp = ros::Time::now();
 
-    goal.target_pose.pose.position.x = 1.0;
-    goal.target_pose.pose.orientation.w = 1.0;
+        goal.target_pose.pose.position.x = 1.0;
+        goal.target_pose.pose.orientation.w = 1.0;
 
-    ROS_INFO("sending goal");
-    ac.sendGoal(goal);
+        ROS_INFO("sending goal");
+        ac.sendGoal(goal);
 
-    ac.waitForResult();
+        ac.waitForResult();
 
-    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-        ROS_INFO("base moved forward");
-    else
-        ROS_INFO("the base failed to move for some reason");
+        if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+            ROS_INFO("base moved forward");
+        else
+            ROS_INFO("the base failed to move for some reason");
+        i ++;
+
     }
-return 0;
+
+    return 0;
 }
